11ebf7dcaa12b27c019aab0ca384121a8f750884
Anupam Anand <67553013+anupam0302@users.noreply.github.com>
2025-11-16 19:44:25 +0530

    Parent pom

---
+++ src/main/java/com/shoposhphere/ShoposphereParentApplication.java
@@ -0,0 +1,13 @@
+package com.shoposhphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoposphereParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoposphereParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoposhphere/ShoposphereParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoposhphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoposphereParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
